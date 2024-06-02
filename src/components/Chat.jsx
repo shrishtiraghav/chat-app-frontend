266a9210dc4e6b0d17ef37ddb94819544853203f
shrishtiraghav <shrishtiraghav1998@gmail.com>
2024-06-02 21:48:03 +0530

    chat component update

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -3,13 +3,12 @@ import axios from 'axios';
 
 const Chat = ({ pdfText }) => {
   const [prompt, setPrompt] = useState('');
-  const [responses, setResponses] = useState([]);
+  const [responses, setResponses] = useState("");
 
   const handleSendMessage = async () => {
     try {
       const response = await axios.post('http://localhost:5000/api/chat/chat', { prompt: pdfText + '\n' + prompt });
-      setResponses([...responses, { prompt, response: response.data.response }]);
-      setPrompt('');
+      setResponses(response.data.response.text);
     } catch (error) {
       console.error('Error sending message', error);
     }
@@ -18,12 +17,10 @@ const Chat = ({ pdfText }) => {
   return (
     <div>
       <div>
-        {responses.map((res, index) => (
-          <div key={index}>
-            <p><strong>You:</strong> {res.prompt}</p>
-            <p><strong>Bot:</strong> {res.response}</p>
+          <div>
+            <p><strong>You:</strong> {prompt}</p>
+            <p><strong>Bot:</strong> {responses}</p>
           </div>
-        ))}
       </div>
       <input
         type="text"
